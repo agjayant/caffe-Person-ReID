f5d21c9450f130b5853b672ce3fac44f37678c3e
agjayant <agjayant@rediff.com>
2016-12-16 02:38:33 +0530

    extracting attributes for market

---
+++ ReID/atrMarket.py
@@ -0,0 +1,235 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+
+# In[2]:
+
+import numpy as np
+#import matplotlib.pyplot as plt
+import math
+#get_ipython().magic(u'matplotlib inline')
+
+# Make sure that caffe is on the python path:
+caffe_root = '../../'
+import sys
+sys.path.insert(0, caffe_root + 'python')
+
+import caffe
+import fileinput
+
+#plt.rcParams['figure.figsize'] = (10, 10)
+#plt.rcParams['image.interpolation'] = 'nearest'
+#plt.rcParams['image.cmap'] = 'gray'
+
+import os
+
+
+# In[3]:
+
+#getDiff Implementation
+def getDiff( vector1, vector2 ):
+    sum=0
+    for j in range(len(vector1)):
+        diff= vector1[j]-vector2[j]
+        diff=diff*diff
+        sum=sum+diff
+
+    return math.sqrt(sum)
+
+
+# In[4]:
+
+
+# In[9]:
+
+##test block
+
+
+# In[ ]:
+
+caffe.set_device(1)
+caffe.set_mode_gpu()
+net = caffe.Net(sys.argv[1], #path to deploy prototxt,
+                sys.argv[2], #path to learned model,
+                caffe.TEST)# input preprocessing: 'data'is the name of the input blob == net.inputs[0]
+
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2,0,1))
+#transformer.set_mean('data', np.load(caffe_root + 'rank_scripts/query_128x128_market.npy').mean(1).mean(1)) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+
+
+# In[ ]:
+
+image_data = []
+label = []
+attributes = []
+
+
+# In[ ]:
+
+# In[5]:
+
+#******************************
+#Forward Pass for  Test Folder
+#******************************
+test_folder = '/users/gpu/agjayant1/Market-1501-v15.09.15/bounding_box_trainx2/'
+# test_folder = '/users/gpu/agjayant1/Market-1501-v15.09.15/query/'
+images_list = os.listdir(test_folder)
+
+print "Setup Done. Starting Forward Pass for Test Images"
+
+# In[6]:
+
+num_images = len(images_list)
+BatchSize = 100
+j=0
+
+while j < 5000:
+    net = caffe.Net(sys.argv[1], # deploy prototxt,
+                sys.argv[2], # model,
+                caffe.TEST)# input preprocessing: 'data'is the name of the input blob == net.inputs[0]
+
+
+    # set net to batch size
+    net.blobs['data'].reshape(BatchSize,3,227,227)
+    i = 0
+    k = j
+    while j < num_images and i < BatchSize:
+        test_image = caffe.io.load_image(test_folder+ images_list[j])
+        net.blobs['data'].data[i] = transformer.preprocess('data', test_image)
+        i = i + 1
+        j = j + 1
+
+    out = net.forward()
+    i=0
+    while k < num_images and i < BatchSize:
+        pic = Image.open(test_folder+images_list[k])
+        A = np.array(pic,dtype='f4')
+        A = A.transpose((2,0,1))
+        image_data.append(A)
+        attributes.append(out['fc8_a'][i])
+        label.append(int(item.split('_')[0]))
+        i = i + 1
+        k = k + 1
+
+
+# In[7]:
+
+print "**************************************************"
+print "Forward Pass Completed for all Test Images: Part 1"
+print "**************************************************"
+label = np.array(label,dtype='f4')
+attributes = np.array(attributes,dtype='f4')
+h = h5py.File("market_training1.h5", 'w' )
+dset1 = h.create_dataset("data", data=image_data)
+dset2 = h.create_dataset("attributes", data=attributes)
+dset3 = h.create_dataset("label",  data=label)
+h.close()
+
+print "**************************************************"
+print "Saved for all Images: Part 1"
+print "**************************************************"
+
+
+# In[ ]:
+
+while j < 10000:
+    net = caffe.Net(sys.argv[1], # deploy prototxt,
+                sys.argv[2], # model,
+                caffe.TEST)# input preprocessing: 'data'is the name of the input blob == net.inputs[0]
+
+
+    # set net to batch size
+    net.blobs['data'].reshape(BatchSize,3,227,227)
+    i = 0
+    k = j
+    while j < num_images and i < BatchSize:
+        test_image = caffe.io.load_image(test_folder+ images_list[j])
+        net.blobs['data'].data[i] = transformer.preprocess('data', test_image)
+        i = i + 1
+        j = j + 1
+
+    out = net.forward()
+    i=0
+    while k < num_images and i < BatchSize:
+        pic = Image.open(test_folder+images_list[k])
+        A = np.array(pic,dtype='f4')
+        A = A.transpose((2,0,1))
+        image_data.append(A)
+        attributes.append(out['fc8_a'][i])
+        label.append(int(item.split('_')[0]))
+        i = i + 1
+        k = k + 1
+
+
+# In[7]:
+
+print "******************************************"
+print "Forward Pass Completed for all Test Images : 2"
+print "******************************************"
+label = np.array(label,dtype='f4')
+attributes = np.array(attributes,dtype='f4')
+h = h5py.File("market_training2.h5", 'w' )
+dset1 = h.create_dataset("data", data=image_data)
+dset2 = h.create_dataset("attributes", data=attributes)
+dset3 = h.create_dataset("label",  data=label)
+h.close()
+
+print "**************************************************"
+print "Saved for all Images: Part 2"
+print "**************************************************"
+
+
+# In[ ]:
+
+while j < num_images:
+    net = caffe.Net(sys.argv[1], # deploy prototxt,
+                sys.argv[2], # model,
+                caffe.TEST)# input preprocessing: 'data'is the name of the input blob == net.inputs[0]
+
+
+    # set net to batch size
+    net.blobs['data'].reshape(BatchSize,3,227,227)
+    i = 0
+    k = j
+    while j < num_images and i < BatchSize:
+        test_image = caffe.io.load_image(test_folder+ images_list[j])
+        net.blobs['data'].data[i] = transformer.preprocess('data', test_image)
+        i = i + 1
+        j = j + 1
+
+    out = net.forward()
+    i=0
+    while k < num_images and i < BatchSize:
+        pic = Image.open(test_folder+images_list[k])
+        A = np.array(pic,dtype='f4')
+        A = A.transpose((2,0,1))
+        image_data.append(A)
+        attributes.append(out['fc8_a'][i])
+        label.append(int(item.split('_')[0]))
+        i = i + 1
+        k = k + 1
+
+
+# In[7]:
+
+print "******************************************"
+print "Forward Pass Completed for all Test Images:3"
+print "******************************************"
+label = np.array(label,dtype='f4')
+attributes = np.array(attributes,dtype='f4')
+h = h5py.File("market_training3.h5", 'w' )
+dset1 = h.create_dataset("data", data=image_data)
+dset2 = h.create_dataset("attributes", data=attributes)
+dset3 = h.create_dataset("label",  data=label)
+h.close()
+
+print "**************************************************"
+print "Saved for all Images: Part 3"
+print "**************************************************"
+
